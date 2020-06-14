61feac16d81cfa3303a2efa9ce698ea3b213080b
Kate <evgen.knyzhenko@gmail.com>
2020-06-15 01:39:58 +0300

    beforeAPIcode

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class ApishkaMainCode extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-export default App;
+  render() {
+    return (
+        <div className='wapper'>
+          <div className='wrap'>
+            <div className= 'hiHeader'>Ну привет, Женя!</div>
+            <div className= 'mainCode'>
+              <div className='divWithButtonCreateTask'>
+              <button className='createTask'>Create task</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+    )
+  }
+}
+export default ApishkaMainCode;
